f7e0f37eb952effb4238c9efdf44dc0a867fd53c
cmuth <clement.muth@epitech.eu>
2020-04-13 15:04:04 +0200

    [ADD] struct.h

---
+++ include/struct.h
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2020
+** lemin_2019 [WSL: Debian]
+** File description:
+** struct
+*/
+
+#ifndef STRUCT_H_
+#define STRUCT_H_
+
+typedef struct anth_s
+{
+    char *anthman;
+    char *position;
+} anth_t;
+
+typedef struct anthill_s
+{
+    anth_t *anth;
+    size_t nanth;
+    int ac;
+    char **anthill;
+    char **startend_room;
+    char **av;
+} anthill_t;
+
+typedef enum near_room_t {next = 0, past = 1};
+
+typedef struct tunnel_s
+{
+    char **near_room;
+} tunnel_t;
+
+
+typedef struct lemin_s
+{
+    anthill_t anthill;
+    char *content;
+} lemin_t;
+
+
+#endif /* !STRUCT_H_ */
