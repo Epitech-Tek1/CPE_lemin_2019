7ccb7d77208bc1cccf8a1a962f962ffd6579b9c8
cmuth <clement.muth@epitech.eu>
2020-04-14 18:53:10 +0200

    [UPDATE] printt.c

--- lib/_string/printt.c
+++ lib/_string/printt.c
@@ -7,10 +7,12 @@
 
 #include "string.h"
 
-void printt_string(char **array)
+void printt_string(char **array, bool jump)
 {
-    for (int i = 0; array[i]; i++)
+    for (int i = 0; array[i]; i++) {
         _putstr(array[i], 1);
+        if (jump) _putchar('\n');
+    }
 }
 
 void printt_number(int *array)
@@ -19,8 +21,8 @@ void printt_number(int *array)
         _putnbr(array[i], 1);
 }
 
-void _printt(const char *restrict format, const void *array)
+void _printt(const char *restrict format, const void *array, bool jump)
 {
-    if (_strcmp(format, "%s")) printt_string((char **)array);
+    if (_strcmp(format, "%s")) printt_string((char **)array, jump);
     else if (_strcmp(format, "%d")) printt_number((int *)array);
 }
