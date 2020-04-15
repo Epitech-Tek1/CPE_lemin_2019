f27944f6f9edda8a8c70eafe2dfd6d7ae039403d
eiermann.patrick.pv@outlook.com <Patrickdu24092001>
2020-04-15 20:47:18 +0200

    maseltof

--- src/parse_file_0.c
+++ src/parse_file_0.c
@@ -13,7 +13,6 @@ static int get_file(lemin_t *lemin)
  
     for (i = 0; read(0, lemin->content + i, 1) > 0 && i < 5000; i++);
     lemin->content[i] = '\0';
-    my_printf("%s\n", lemin->content);
     return (NORMAL_EXIT);
 }
 
