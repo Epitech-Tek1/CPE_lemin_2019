19166e28e1e8855fab9384aadb558ffffd186b6f
cmuth <clement.muth@epitech.eu>
2020-04-15 14:10:21 +0200

    [EDIT] lib/my/getnbr.c

--- lib/my/my_str_isalpha.c
+++ lib/my/my_str_isalpha.c
@@ -12,7 +12,8 @@ int	my_str_isalpha(char const *str)
     int i = 0;
 
     while (str[i] != '\0') {
-        if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' &&  str[i] <= 'z'))
+        if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' &&  str[i] <=
+        'z'))
             i++;
         else
             return (0);
