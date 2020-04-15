02efdc38a9286d2ed7ca5249ee480445da314e82
Timothe Rominger <timothe.rominger@epitech.eu>
2020-04-15 18:33:01 +0200

    correction error

--- src/output.c
+++ src/output.c
@@ -10,29 +10,29 @@
 int display_rooms(lemin_t *lemin)
 {
     for (int i = 0; lemin->anthill.anthill[i] != NULL; i++)
-        my_printf(lemin->anthill.anthill[i], 1);
+        my_printf("%s\n", lemin->anthill.anthill[i]);
     return (EXIT_SUCCESS);
 }
 
 int display_tunnels(lemin_t *lemin)
 {
     for (int i = 0; lemin->tunnel.near_room[i] != NULL; i++)
-        my_printf(lemin->tunnel.near_room[i], 1);
+        my_printf("%s\n", lemin->tunnel.near_room[i]);
     return (EXIT_SUCCESS);
 }
 
 int display_output(lemin_t *lemin)
 {
-    my_printf("#number_of_ants\n", 1);
+    my_printf("#number_of_ants\n");
     my_printf("%ld\n", lemin->anthill.nanth);
-    my_printf("#rooms\n", 1);
-    my_printf("##start\n", 1);
-    my_printf(lemin->anthill.startend_room[0], 1);
-    my_printf("##end\n", 1);
-    my_printf(lemin->anthill.startend_room[1], 1);
+    my_printf("#rooms\n");
+    my_printf("##start\n");
+    my_printf("%s\n", lemin->anthill.startend_room[0]);
+    my_printf("##end\n");
+    my_printf("%s\n", lemin->anthill.startend_room[1]);
     display_rooms(lemin);
-    my_printf("#tunnels\n", 1);
+    my_printf("#tunnels\n");
     display_tunnels(lemin);
-    my_printf("#moves\n", 1);
+    my_printf("#moves\n");
     return (EXIT_SUCCESS);
 }
