182cd490238979802273074b18ed2622e66e979d
cmuth <clement.muth@epitech.eu>
2020-04-17 19:27:08 +0200

    [ADD] *

---
+++ include/library/_maths/include/maths.h
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2020
+** library
+** File description:
+** maths
+*/
+
+#ifndef MATHS_H_
+#define MATHS_H_
+
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+#include "maths_struct.h"
+#include "maths_define.h"
+#include "maths_function.h"
+#include "../../_string/include/string.h"
+
+#endif /* !MATHS_H_ */
---
+++ include/library/_maths/include/maths_struct.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** library
+** File description:
+** maths_struct
+*/
+
+#ifndef MATHS_STRUCT_H_
+#define MATHS_STRUCT_H_
+
+typedef struct vector2i_s
+{
+    int x, y;
+} vector2i;
+
+#endif /* !MATHS_STRUCT_H_ */
